2f7c4569977e9c7cdc0b09fc0f1f69cab7cbe43f
vpgm <chenh_2996@163.com>
2020-03-23 05:10:20 +0800

    develop module pages

---
+++ pages/tv.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <mt-header fixed title="剧集">
+      <nuxt-link to="/" slot="left">
+        <mt-button icon="back"></mt-button>
+      </nuxt-link>
+    </mt-header>
+    <div class="content-wrapper">功能正在开发中.....</div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
